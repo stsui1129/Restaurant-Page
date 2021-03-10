9a34da5a2f4864fcfda7f350cfc7be23fe9aec05
Sheng <s.tsui@outlook.com>
2021-03-10 18:39:38 +1300

    added modules

---
+++ src/modules/home.js
@@ -0,0 +1,8 @@
+const homePage = function () {
+    const contentDiv = document.querySelector("#content");
+
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("restaurant-image");
+    const backgroundImage = document.createElement("img");
+    backgroundImage.setAttribute("src", /images/restaurantpicture.jpeg )
+}
