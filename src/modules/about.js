3d2ef5eb86cb84434d4d45e0d410cfba4a208bae
Sheng <s.tsui@outlook.com>
2021-03-13 20:31:11 +1300

    updated

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,8 +1,12 @@
-const homePage = function () {
+const aboutPage = function () {
     const contentDiv = document.querySelector("#content");
 
     const imageDiv = document.createElement("div");
     imageDiv.classList.add("restaurant-image");
     const backgroundImage = document.createElement("img");
-    backgroundImage.setAttribute("src", /images/restaurantpicture.jpeg )
+    backgroundImage.setAttribute("src", /images/restaurantpicture.jpeg);
+
+
+    contentDiv.appendChild(imageDiv);
+    imageDiv.appendChild(backgroundImage);
 }
