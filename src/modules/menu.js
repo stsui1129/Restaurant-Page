24f1c957af0173abf528bad4c15479b63751f04d
Sheng <s.tsui@outlook.com>
2021-03-17 20:29:01 +1300

    added menu image

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,8 +5,14 @@ const loadMenu = function() {
     menuDiv.classList.add("menu-content");
     contentDiv.appendChild(menuDiv);
 
+    const bubbletea = document.createElement("img");
+    bubbletea.classList.add("bubbletea-image");
+    bubbletea.setAttribute("src", "./images/bubbleteamenu.jpeg");
+    bubbletea.setAttribute("alt", "menu of bubble tea");
+    menuDiv.appendChild(bubbletea);
+
     const para = document.createElement("p");
-    para.classList.add("menu-para")
+    para.classList.add("menu-para");
     para.textContent = "Lorem ipsum this is just random text for menu tab";
     menuDiv.appendChild(para);
 }
